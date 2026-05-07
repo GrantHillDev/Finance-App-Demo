1d1273bc067c65c99104b4df294e020b1648e12f
Grant Hill <granthill@live.com>
2026-05-06 22:42:24 -0400

    AGH Finance App Demo 01 commit.

---
+++ Controllers/FinanceController.cs
@@ -0,0 +1,34 @@
+﻿using  Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FinanceController : ControllerBase
+    {
+        [HttpPost("double")]//This presumably represents a first 'endpoint' in my API project (POST Api/finance/double).
+        public IActionResult DoubleNumber([FromBody] InputModel input)
+        {
+            var result = input.Number * 2;
+            return Ok(result);
+        }
+    }
+
+    public class InputModel
+    {
+        public int Number { get; set; }
+    }
+}
+
+
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace FinanceAppDemo01.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class FinanceController : ControllerBase
+//    {
+//    }
+//}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿function sendData()
+{
+    const value = document.getElementById("numberInput").value;
+    
+
+    fetch('https://localhost:44324/api/finance/double',
+    {
+        method: 'POST',
+        headers:
+        {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({number: parseInt(value)})
+
+    })
+    .then(response => response.json())
+    .then(data =>
+    {
+        document.getElementById("result").innerText = data;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
