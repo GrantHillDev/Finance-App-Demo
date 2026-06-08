20df0f34b587786412a4d6362511f8b96203861a
Grant Hill <granthill@live.com>
2026-06-07 21:04:53 -0400

    AGH Finance App Demo 01 third commit.

--- Controllers/FinanceController.cs
+++ Controllers/FinanceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace FinanceApp.Controllers
 {
@@ -14,20 +15,78 @@ namespace FinanceApp.Controllers
         }
 
         [HttpPost("divideInHalf")]//Second endpoint?
-        public IActionResult DivideNumber([FromBody] InputModel input)
+        public IActionResult DivideNumber([FromBody] InputModel input)//Returning a variable.
         {
             var result = input.Number / 2;
             return Ok(result);
         }
-    }
 
+        [HttpPost("calculateMortgage")]//Another endpoint.
+        public IActionResult CalculateMortgage([FromBody]MortgageInputModel input)//Returning an object.
+        {
+            double monthlyInterestRate =
+                (input.InterestRate / 100) / 12;
+            int totalPayments =
+                input.LoanTerm * 12;
+            double principal =
+                input.LoanAmount;
+            double monthlyPrincipalAndInterest =
+                principal *
+                (monthlyInterestRate *
+                Math.Pow(1 + monthlyInterestRate,
+                totalPayments))
+                /
+                (Math.Pow(1 + monthlyInterestRate,
+                totalPayments) - 1);
+            double monthlyTaxes =
+                input.PropertyTaxes / 12;
+            double monthlyInsurance =
+                input.HomeInsurance / 12;
+            double totalMonthlyPayment =
+                monthlyPrincipalAndInterest +
+                monthlyTaxes +
+                monthlyInsurance +
+                input.PMI +
+                input.HOA;
 
-    public class InputModel//it's looking like we can use the same class for multiple endpoints.... in the same way you can use the same variable name in multiple methods, so long as each is kept within each method's individual scope, mutually exclusive of each other.
-    {
-        public int Number { get; set; }
+            return Ok(new
+            {
+                monthlyPayment =
+                    totalMonthlyPayment,
+                monthlyTaxes,
+                monthlyInsurance,
+                totalPayments
+            });
+        }
+        
+        public class MortgageInputModel//creating an object. Building this because multiple values are being sent in one request -- think it'd make the most sense to provide the user with the easiest means of input, so let them do it once, rather than multiple times for the same result.
+        {
+            public double HomeMarketValue { get; set; }
+            public double DownPayment {  get; set; }
+            public double LoanAmount { get; set; }
+            public double InterestRate { get; set; }
+            public int LoanTerm { get; set; }
+            public double PropertyTaxes { get; set; }
+            public double PMI { get; set; }
+            public double HomeInsurance { get; set; }
+            public double HOA { get; set; }
+            public string LoanStartMonth { get; set; }
+            public int LoanStartYear { get; set; }
+            public string LoanType { get; set; }
+        }
+
+        public class InputModel//it's looking like we can use the same class for multiple endpoints.... in the same way you can use the same variable name in multiple methods, so long as each is kept within each method's individual scope, mutually exclusive of each other.
+        {
+            public int Number { get; set; }
+        }
     }
 }
-
+//[HttpPost("addHomeMarketValue")]
+//public IActionResult CalculateHomeMortage([FromBody] InputModel input)
+//{
+//    var result = input.Number;
+//    return Ok(result);
+//}
 
 //using Microsoft.AspNetCore.Http;
 //using Microsoft.AspNetCore.Mvc;
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -42,7 +42,60 @@ function divideNumber()//to push over multiple lines at once in visual studio, s
         .catch(error => console.error('Error:', error));
 }
 
+function calculateMortgage()//actually, this is an object made in javascript.... a json object? Oh.... json is an acronym for javascript object notation.... because I thought javascript wasn't necessarily an object oriented language?
+{
+    const mortgageData = //didn't know that you could create multiple fields of variables with stored data in them all to equate to one const. Like an on object in C#....
+    {
+            homeMarketValue: parseFloat (document.getElementById("homeMarketValue").value),//what's a floating point number?
+            downPayment: parseFloat (document.getElementById("downPayment").value),//naming the variable here in javascript the same thing as what it's retrieving from the input id attribute name (that's already been written in my html component) makes this so much simpler.
+            loanAmount: parseFloat (document.getElementById("loanAmount").value),
+            interestRate: parseFloat (document.getElementById("interestRate").value),
+            loanTerm: parseFloat (document.getElementById("loanTerm").value),
+            propertyTaxes: parseFloat (document.getElementById("propertyTaxes").value),//guessing that the home renovation input from my html file wasn't written in here within javascript because it wasn't important? Or enough to be considered as a worthy part of this whole mortgage calculation?
+            pmi: parseFloat (document.getElementById("privateMortgageInsurance").value),
+            homeInsurance: parseFloat (document.getElementById("homeOwnerInsurance").value),
+            hoa: parseFloat (document.getElementById("hOA").value),
+            loanStartMonth: parseFloat (document.getElementById("loanStartMonth").value),
+            loanStartYear: parseFloat (document.getElementById("loanStartYear").value),
+            loanType: parseFloat (document.getElementById("loanType").value),
+    }
+    fetch('https://localhost:44324/api/finance/calculateMortgage',
+    {
+        method: 'POST',
+        headers:
+        {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(mortgageData)
+    })
+    .then(response => response.json())
+    .then(data =>
+    {
+        document.getElementById("monthlyPaymentResult").innerText = data//console.log(data);//This prints the results of the data to the console.
+        document.getElementById("monthlyTaxResult").innerText = data
+        document.getElementById("monthlyInsuranceResult").innerText = data
+    })
+    .catch(error =>
+        console.error('Error:', error));
+}
+// function submitHomeMarketValue()
+// {
+//     const value = document.getElementById("homeMarketValue").value;
 
+//     fetch('https://localhost:44324/api/finance/addHomeMarketValue',
+//         {
+//             method: 'POST',
+//             headers:
+//             {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({number: parseInt(value)})
+//         })
+//         .then(response => response.json())
+//         .then(data =>
+//         )
+//         .catch(error => console.error('Error:', error));
+// }
 
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
